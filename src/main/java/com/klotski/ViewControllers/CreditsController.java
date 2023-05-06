65d056e902b7de30f965306b39402aaf287b3074
FrancescoAA1 <francescofantin2002@gmail.com>
2023-05-06 15:01:08 +0200

    Interfacce Crediti e SavedGames

--- src/main/java/com/klotski/ViewControllers/CreditsController.java
+++ src/main/java/com/klotski/ViewControllers/CreditsController.java
@@ -1,7 +1,14 @@
 package com.klotski.ViewControllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -11,5 +18,26 @@ public class CreditsController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         System.out.println("OK");
+    }
+
+    public void MenuClicked(ActionEvent actionEvent)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/klotski/View/menu.fxml"));
+        OpenWindow(fxmlLoader, "Main Menu", actionEvent);
+    }
+
+    private void OpenWindow(FXMLLoader fxmlLoader, String title, ActionEvent event)
+    {
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
     }
 }
