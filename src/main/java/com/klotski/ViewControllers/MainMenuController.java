f1db15f3a6c9dc97935ee9a11e0e4f8ed14d8519
Tomas Lovato <59308506+LovatoTomas@users.noreply.github.com>
2023-05-06 09:40:26 +0200

    Linked MainMenu
    Added link from MainMenu to other pages.
    Improved disposition cards animations.

--- src/main/java/com/klotski/ViewControllers/MainMenuController.java
+++ src/main/java/com/klotski/ViewControllers/MainMenuController.java
@@ -2,26 +2,66 @@ package com.klotski.ViewControllers;
 
 import com.klotski.UI.DispositionCard;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
-    /*@FXML
-    private Label welcomeText;
-
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }*/
+    private GridPane menuGrid;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
         System.out.println("OK1");
+    }
+    public void StartGameClicked(MouseEvent event)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/klotski/View/dispositions.fxml"));
+        OpenWindow(fxmlLoader, "Dispositions", event);
+    }
+
+    public void LoadGameClicked(MouseEvent event)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/klotski/View/saved.fxml"));
+        OpenWindow(fxmlLoader, "Saved Games", event);
+    }
+
+    public void CreditsClicked(MouseEvent event)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/klotski/View/credits.fxml"));
+        OpenWindow(fxmlLoader, "Credits", event);
+    }
+
+    public void ExitClicked(MouseEvent event)
+    {
+        Stage stage = (Stage) menuGrid.getScene().getWindow();
+        stage.close();
+    }
+
+    private void OpenWindow(FXMLLoader fxmlLoader, String title, MouseEvent event)
+    {
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle(title);
+        stage.setScene(scene);
+        stage.show();
     }
 }
