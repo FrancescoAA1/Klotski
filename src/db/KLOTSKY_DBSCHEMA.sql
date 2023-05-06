376056d1e1b6515ffe963b0d86b04ccb39083e16
Mattia Galassi <mattiagalassi02@gmail.com>
2023-05-06 18:29:14 +0200

    DB created and added to his folder (see also query to recreate))

--- src/db/KLOTSKY_DBSCHEMA.sql
+++ src/db/KLOTSKY_DBSCHEMA.sql
@@ -1,14 +1,17 @@
-CREATE TABLE `DISPOSITION` (
-  `disposition_id` integer PRIMARY KEY,
-  `schema` varchar(255),
-  `original` boolean
+CREATE TABLE "DISPOSITIONS"
+(
+    "disposition_id" INTEGER NOT NULL UNIQUE,
+    "schema"         TEXT    NOT NULL UNIQUE,
+    "original"       INTEGER NOT NULL,
+    PRIMARY KEY ("disposition_id")
 );
 
-CREATE TABLE `MATCHES` (
-  `match_id` integer PRIMARY KEY,
-  `name` varchar(255),
-  `last_disposition` integer,
-  `score` integer
+CREATE TABLE "MATCHES"
+(
+    "match_id"    INTEGER NOT NULL UNIQUE,
+    "name"        TEXT    NOT NULL UNIQUE,
+    "disposition" INTEGER NOT NULL,
+    "score"       INTEGER NOT NULL,
+    PRIMARY KEY ("match_id" AUTOINCREMENT),
+    FOREIGN KEY ("disposition") REFERENCES "DISPOSITIONS" ("disposition_id")
 );
-
-ALTER TABLE `DISPOSITION` ADD FOREIGN KEY (`disposition_id`) REFERENCES `MATCHES` (`last_disposition`);
