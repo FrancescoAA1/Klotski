1be11d9f6779da7c8e112fb129f6702f2b62c07e
FrancescoAA1 <francescofantin2002@gmail.com>
2023-05-07 19:06:45 +0200

    Todo Udpate for View Saved Games Added from Code Behind

---
+++ src/main/java/com/klotski/UI/SavedGame.java
@@ -0,0 +1,143 @@
+package com.klotski.UI;
+
+import com.klotski.ViewControllers.GameController;
+import javafx.animation.FadeTransition;
+import javafx.animation.ScaleTransition;
+import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.text.SimpleDateFormat;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+public class SavedGame {
+    private String ImagePath;
+    private int moveNumber;
+
+    private Date gameDate;
+
+    private boolean gameState;
+
+
+    private Pane control;
+
+    public SavedGame(String imagePath, int move_n, Date game_d, boolean game_s) {
+        ImagePath = imagePath;
+        moveNumber = move_n;
+        gameDate = game_d;
+        gameState = game_s;
+        control = GenerateControl();
+    }
+
+    public void SetImagePath(String imagePath) {
+        ImagePath = imagePath;
+    }
+
+    public String GetImagePath() {
+        return ImagePath;
+    }
+
+    public void SetMoveNumber(int m) {
+        moveNumber = m;
+    }
+    public int GetMoveNumber() { return moveNumber; }
+
+    public void SetGameDate(Date game) {
+        gameDate = game;
+    }
+    public Date GetGameDate() { return gameDate; }
+
+    public void SetGameState(boolean state) {
+        gameState = state;
+    }
+    public boolean GetGameState() { return gameState; }
+
+
+    private Pane GenerateControl()
+    {
+        // Container
+        Pane game = new Pane();
+        game.setPrefWidth(200);
+        game.setPrefHeight(200);
+        game.setOnMouseClicked(e -> onMouseClicked(e));
+
+        game.setOpacity(0.75);
+
+        // Disposition ImageView
+        File file = new File(ImagePath);
+        ImageView img = new ImageView(new Image(file.toURI().toString()));
+        img.setFitHeight(80);
+        img.setFitWidth(60);
+        img.setLayoutX(25);
+        img.setLayoutY(5);
+
+        // DispositionNumber Label
+        Label lbl_mov = new Label("NUMERO DI MOSSE: " + String.valueOf(moveNumber));
+        lbl_mov.setLayoutX(109);
+        lbl_mov.setLayoutY(14);
+        lbl_mov.getStyleClass().add("label");
+
+        SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+
+        Label lbl_date = new Label("DATA: " + formatter.format(gameDate));
+        lbl_date.setLayoutX(109);
+        lbl_date.setLayoutY(37);
+        lbl_date.getStyleClass().add("label");
+
+        String state = "";
+
+        if(gameState){
+            state = "TERMINATA";
+        }
+        else
+        {
+            state = "NON TERMINATA";
+        }
+        Label lbl_state = new Label("STATO PARTITA: " + state);
+        lbl_state.setLayoutX(109);
+        lbl_state.setLayoutY(59);
+        lbl_state.getStyleClass().add("label");
+
+
+        // Add children
+        game.getChildren().add(lbl_state);
+        game.getChildren().add(lbl_mov);
+        game.getChildren().add(lbl_date);
+        game.getChildren().add(img);
+
+        return game;
+    }
+
+    public Pane getControl()
+    {
+        return control;
+    }
+
+    private void onMouseClicked(Event event)
+    {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/klotski/View/boardgame.fxml"));
+
+        Parent root = null;
+        try {
+            root = fxmlLoader.load();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setTitle("Klotski");
+        stage.setScene(scene);
+        stage.show();
+    }
+}
--- src/main/java/com/klotski/ViewControllers/SavedController.java
+++ src/main/java/com/klotski/ViewControllers/SavedController.java
@@ -1,23 +1,36 @@
 package com.klotski.ViewControllers;
 
+import com.klotski.UI.DispositionCard;
+import com.klotski.UI.SavedGame;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class SavedController implements Initializable {
 
+    @FXML
+    private GridPane grid;
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle)
     {
-        System.out.println("OK");
+        SavedGame s = new SavedGame(getClass().getResource("/com/klotski/Images/m1.png").getPath(), 23, new Date(2023, 8, 2), true);
+        SavedGame s1 = new SavedGame(getClass().getResource("/com/klotski/Images/m2.png").getPath(), 23, new Date(2023, 8, 2), true);
+        grid.add(s.getControl(), 0,2);
+        grid.add(s1.getControl(), 0,3);
     }
 
     public void MenuClicked(ActionEvent actionEvent)
