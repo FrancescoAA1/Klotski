11c8b6f54865fbf882a421eb9c045551b17bff37
Mattia Galassi <mattiagalassi02@gmail.com>
2023-05-15 23:58:06 +0200

    Added methods to update match and to get the id of a match searched by name

--- src/main/java/com/klotski/Controllers/DBConnector.java
+++ src/main/java/com/klotski/Controllers/DBConnector.java
@@ -29,7 +29,8 @@ public class DBConnector {
      * Constructor
      * I need to obtain the current working directory
      */
-    public DBConnector() {
+    public DBConnector()
+    {
         // get the current directory
         try {
             currentPath = new java.io.File(".").getCanonicalPath();
@@ -41,7 +42,8 @@ public class DBConnector {
     /**
      * Connect to local DB if the file dedicated exists
      */
-    public void connect() {
+    public void connect()
+    {
         // check if DB file exists...otherwise throw an error
         // create a Path to check in filesystem the existence
 
@@ -62,7 +64,8 @@ public class DBConnector {
     /**
      * Close the DB connection if it was opened
      */
-    public void close() {
+    public void close()
+    {
         try {
             if (connector != null && !connector.isClosed())
                 connector.close();
@@ -74,9 +77,11 @@ public class DBConnector {
      * This method is reserved to insert a new match in the DB
      *
      * @param match: the match to be saved
+     * @param disposition: the disposition of this match
      * @return TRUE: if the DB INSERTION is correct, otherwise FALSE
      */
-    public boolean saveMatch(Match match, Disposition disposition) {
+    public boolean saveMatch(Match match, Disposition disposition)
+    {
         // if is not already connected to DB
         if (connector == null)
             connect();
@@ -104,18 +109,9 @@ public class DBConnector {
         // I sort the table in descending order by ID so
         // the value with the largest ID is the first (and it's the last inserted).
         // Then, I extract only the first row from the SELECT
-        String querysql2 = "SELECT disposition_id FROM DISPOSITIONS ORDER BY disposition_id DESC LIMIT 1";
-        int lastDispositionID;
-        try {
-            Statement statement = connector.createStatement();
-            // executes the DB SELECT and the result is saved in result record collection
-            ResultSet result = statement.executeQuery(querysql2);
-
-            lastDispositionID = result.getInt("disposition_id");
-
-        } catch (SQLException e) {
-            return false;
-        }
+        int lastDispositionID = lastSavedDispositionID();
+        // if it is equals to zero something went wrong
+        if(lastDispositionID == 0) return false;
 
         // Now I have to insert the ID  in the match
 
@@ -145,7 +141,8 @@ public class DBConnector {
      * @return the list of Pair (Match, LastDisposition_ID) saved
      * The ArrayList is null if something goes wrong
      */
-    public ArrayList<Pair<Match, Integer>> listAllRecordedMatches() {
+    public ArrayList<Pair<Match, Integer>> listAllRecordedMatches()
+    {
         ArrayList<Pair<Match, Integer>> matches = null;
 
         // if is not already connected to DB
@@ -185,7 +182,8 @@ public class DBConnector {
      * @return the disposition with the specified ID in DB, null if there is not a disposition
      * with the specified ID
      */
-    public Disposition getDisposition(Integer disposition_id) {
+    public Disposition getDisposition(Integer disposition_id)
+    {
         Disposition disposition = null;
         // if is not already connected to DB
         if (connector == null)
@@ -223,7 +221,8 @@ public class DBConnector {
      * @return the list of all original dispositions
      * The ArrayList is null if something goes wrong
      */
-    public ArrayList<Disposition> listAllOriginalDispositions() {
+    public ArrayList<Disposition> listAllOriginalDispositions()
+    {
         ArrayList<Disposition> originalDisposition = null;
 
         // if it is not already connected to DB
@@ -254,14 +253,16 @@ public class DBConnector {
     }
 
     /**
-     * This method is reserved to return the ID of teh last match saved in DB
+     * This method is reserved to return the ID of the last match saved in DB
      *
      * @return the last match's ID saved on DB
      * 0 if something goes wrong
      */
     public int lastSavedMatchID()
     {
-
+        // if it is not already connected to DB
+        if (connector == null)
+            connect();
         // I sort the MATCHES table in descending order by ID so
         // the value with the largest ID is the first (and it's the last inserted).
         // Then, I extract only the first row from the SELECT
@@ -280,8 +281,163 @@ public class DBConnector {
         } catch (SQLException e) {
             return 0;
         }
+    }
+    /**
+     * This method is reserved to return the ID of the last disposition saved in DB
+     *
+     * @return the last disposition's ID saved on DB
+     * 0 if something goes wrong
+     */
+    public int lastSavedDispositionID()
+    {
+        // if it is not already connected to DB
+        if (connector == null)
+            connect();
+        String querysql = "SELECT disposition_id FROM DISPOSITIONS ORDER BY disposition_id DESC LIMIT 1";
+        int lastDispositionID;
+        try {
+            Statement statement = connector.createStatement();
+            // executes the DB SELECT and the result is saved in result record collection
+            ResultSet result = statement.executeQuery(querysql);
 
+            lastDispositionID = result.getInt("disposition_id");
+
+            return lastDispositionID;
+
+        } catch (SQLException e) {
+            return 0;
+        }
     }
 
+    /**
+     * This method is reserved to return the ID of the match specified in params
+     * if it is saved in DB
+     * @param match: the game whose ID you want to get
+     * @return the match ID if it is saved in DB
+     * 0 if something goes wrong or match is not in DB
+     */
+    public int getMatchID(Match match)
+    {
+        // if it is not already connected to DB
+        if (connector == null)
+            connect();
+        // I want to search with a SELECTION QUERY the id associated to the match with this name
+        String querysql = "SELECT match_id FROM MATCHES WHERE name = ?";
+
+        try
+        {
+            PreparedStatement statement = connector.prepareStatement(querysql);
+            //insert the param from match (name)
+            statement.setString(1, match.getName());
+            // executes the DB SELECT and the result is saved in result record collection
+            ResultSet result = statement.executeQuery();
+            // It returns only one object (the ID if is present)
+            return result.getInt("match_id");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
+    /**
+     * This method is reserved to update an already existing match in the DB
+     *
+     * @param match: the match to be updated
+     * @param disposition: the disposition to update associated to the match to update
+     * @return TRUE: if the DB UPDATE is correct, otherwise FALSE
+     */
+    public boolean updateMatch(Match match, Disposition disposition)
+    {
+        // if is not already connected to DB
+        if (connector == null)
+            connect();
+        // the steps are:
+        // (1) Get the id of the match to update
+        // (2) Update the match with new data
+        // (3) Get the ID of the disposition associated to the match
+        // (4) Update the disposition
+
+        // step 1 - Get the id of the match to update
+        int matchID = getMatchID(match);
+        // if it is equals to zero something went wrong
+        if(matchID == 0) return false;
+
+        // step 2 - update the match row
+
+        // the field ID , name and disposition are not changed
+        // so I only need to update score and terminated status
+        String querysql1 = "UPDATE MATCHES SET score = ?, terminated = ? WHERE match_id = ?";
+
+        try {
+            PreparedStatement statement = connector.prepareStatement(querysql1);
+            statement.setInt(1, match.getScore());
+            statement.setInt(2, match.isTerminated()?1:0);
+            statement.setInt(3, matchID);
+            // run query
+            int rowsAffected = statement.executeUpdate();
+            // if no rows where affected by the update => something went wrong
+            if(rowsAffected==0) return false;
+
+        } catch (SQLException e) {
+            return false;
+        }
+
+        // step 3 - Get the ID of the disposition associated to the match
+        int dispositionID = getDispositionAssociated(matchID);
+        // if it is equals to zero something went wrong
+        if(dispositionID == 0) return false;
+
+        // step 4 - Update the disposition
+
+        // the field ID, originals and disposition_image are not changed
+        // so I only need to update the schema
+        String querysql2 = "UPDATE DISPOSITIONS SET schema = ? WHERE disposition_id = ?";
+
+        try {
+            PreparedStatement statement = connector.prepareStatement(querysql2);
+            statement.setString(1, disposition.getTextDisposition());
+            statement.setInt(2, dispositionID);
+            // run query
+            int rowsAffected = statement.executeUpdate();
+            // if no rows where affected by the update => something went wrong
+            if(rowsAffected==0) return false;
+
+        } catch (SQLException e) {
+            return false;
+        }
+
+        return true;
+    }
+    /**
+     * This method is reserved to return the ID of disposition associated
+     * to the match with the ID specified in params
+     * if th eID is correct
+     * @param match_id: the ID of the match whose disposition ID you want to get
+     * @return the disposition ID associated to the match if it is saved in DB
+     * 0 if something goes wrong or match_id is not correct
+     */
+    private int getDispositionAssociated(int match_id)
+    {
+        // if it is not already connected to DB
+        if (connector == null)
+            connect();
+        // I want to search with a SELECTION QUERY the id associated to the match with this name
+        String querysql = "SELECT disposition FROM MATCHES WHERE match_id = ?";
+
+        try
+        {
+            PreparedStatement statement = connector.prepareStatement(querysql);
+            //insert the param from match (name)
+            statement.setInt(1, match_id);
+            // executes the DB SELECT and the result is saved in result record collection
+            ResultSet result = statement.executeQuery();
+            // It returns only one object (the ID if is present)
+            return result.getInt("disposition");
+
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return 0;
+        }
+    }
 }
 
