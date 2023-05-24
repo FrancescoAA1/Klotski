36c39dc641eb29f02da582486f6eea14fe2cdff3
Mattia Galassi <mattiagalassi02@gmail.com>
2023-05-24 23:44:50 +0200

    Updates for API connection
    Da sistemare la dipendenza per usare JSON

--- src/main/java/com/klotski/Controllers/NextMoveGateway.java
+++ src/main/java/com/klotski/Controllers/NextMoveGateway.java
@@ -3,10 +3,13 @@ package com.klotski.Controllers;
 import com.klotski.Model.Disposition;
 import com.klotski.Model.Move;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Represents the object used to obtain the next move based on the actual config
@@ -24,31 +27,73 @@ public class NextMoveGateway
     private static final String PARAM_STARTER = "?";
     private static final String PARAM_SEPARATOR = "&";
     private static final String KEY_VALUE_SEP = "=";
+    private static final String RESPONSE_KEY = "ans";
+    private static final String API_ERROR_VALUE = "0";
+    private static final int HTTP_OK = 200;
 
     // I don't need a Constructor -> I use the default one
 
     /**
      * This method is reserved to obtain the next Move to solve the current disposition
      * This method invoke an HTTP API REST (GET method) located on AWS
-     * @return the next suggested move to solve the passed disposition
+     * @param dispositionNumber: the identifier of the initial configuration from which you started
+     * @param actualDisposition: the actual string representation of the disposition
+     * @return the next suggested move to solve the passed disposition if the current disposition is present
+     * otherwise returns null
      */
     public Move GetNextMove(int dispositionNumber, Disposition actualDisposition)
     {
+        // I want to connect to the API located on AWS
         try
         {
+            // encode in the correct format the string disposition representation
+            String schema = URLEncoder.encode(actualDisposition.getTextDisposition(), StandardCharsets.UTF_8);
+            //prepare the API endpoint with all params
             String formattedUrl = API_URL + PARAM_STARTER +
                     FIRST_PARAM_NAME+KEY_VALUE_SEP+
-                    dispositionNumber+PARAM_SEPARATOR+actualDisposition.getTextDisposition();
-            URL url = new URL(API_URL);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    dispositionNumber+PARAM_SEPARATOR+
+                    SECOND_PARAM_NAME+KEY_VALUE_SEP+schema;
+            URL url = new URL(formattedUrl);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            // Set the REST Method
+            connection.setRequestMethod("GET");
+            // exec the query and save the HTTP response code
+            int responseCode = connection.getResponseCode();
+            // test the response code: if 502 error -> I return null
+            // if it's 200 the query was successful then I fetch the data
+
+            if(responseCode == HTTP_OK)
+            {
+                // estract data from Body response
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuffer response = new StringBuffer();
+
+                while ((inputLine = in.readLine()) != null)
+                {
+                    response.append(inputLine);
+                }
+                in.close();
+
+                // the response is a JSON and I want to convert it with the related library
+
+                ObjectMapper mapper = new ObjectMapper();
+                JsonNode jsonNode = mapper.readTree(jsonString);
+
+                String textNextMove = jsonNode.get(RESPONSE_KEY).asText();
+
+                if(!textNextMove.equals(API_ERROR_VALUE))
+                {
+                    return Move.convertToMove(textNextMove);
+                }
+                else return null;
+            }
+            else return null;
         }
         catch(IOException e)
         {
-
+            return null;
         }
-
-
-        return null;
     }
 
 
--- src/main/java/com/klotski/MainMenu.java
+++ src/main/java/com/klotski/MainMenu.java
@@ -1,5 +1,7 @@
 package com.klotski;
 
+import com.klotski.Controllers.NextMoveGateway;
+import com.klotski.Model.Disposition;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.geometry.Rectangle2D;
@@ -35,6 +37,10 @@ public class MainMenu extends Application {
     }
 
     public static void main(String[] args) {
+        NextMoveGateway next = new NextMoveGateway();
+        Disposition disp = new Disposition(false);
+        disp.setTextDisposition("2-1-0;0#2-2-0;3#2-1-2;0#2-1-1;0#1-2-2;3#2-1-3;0#1-1-1;2#1-1-3;2#1-1-2;4#1-1-3;4#1-1-0;2#1-1-2;2");
+        next.GetNextMove(1, disp);
         launch();
     }
 }
